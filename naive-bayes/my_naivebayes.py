3a28a3dd3d3e6508409d062db39bfae636f27d0e
Danilo César R G de Medeiros <danilocrgm@gmail.com>
2019-05-21 00:20:07 -0300

    Código reorganizado

--- naive-bayes/my_naivebayes.py
+++ naive-bayes/my_naivebayes.py
@@ -7,23 +7,18 @@ from sklearn.utils import shuffle
 # posterior =  --------------------
 #                  evidence
 
-
 # prior = probabilidade de ser a classe  Ck.
 # likelihood = probabilide da caracterísitica v para a classe Ck
-
-
 
 ####################################################################################
 # Esta função calcula os valores da média e da variância para cada característica, #
 # para serem posteriormente utilizadas para o cálculo da densidade probabilistica. #
 ####################################################################################
 def fit(dataset):
-
 	not_in = ["Classe","Idade","Id"] # features que não serão utilizadas
 	data = []
 	for col in dataset.columns:      # varre a tabela e calcula a média e a variância para cada característica
 		if col not in not_in :
-
 			data.append(dataset[col].mean())
 			data.append(dataset[col].var())
 
@@ -31,26 +26,19 @@ def fit(dataset):
 
 #######################################################################################################################
 # Esta função irá de fato criar o modelo baseado no Gaussian Naive Bayes, que se baseia pela densidade probabilistica #
-# de cada características. São separados um número igual das duas classes para manter um balanço do set de treino. 	  #
+# de cada característica. É separado um número igual das duas classes para manter um balanço do set de treino. 	  #
 #######################################################################################################################
-def constroy_model(dataset):
-
-
+def build_model(dataset):
 	strikers = dataset[dataset['Classe'] == 'Atacante']
 	defensors = dataset[dataset['Classe'] == 'Defensor']
-
 	strikers = shuffle(strikers)
 	defensors = shuffle(defensors)
 	train_set1 = strikers	# seleciona uma quantidade da classe Atacante(30).
 	train_set2 = defensors # seleciona uma quantidade da classe Defensor(30).
-
 	data = []
-	#print(len(test_set))
 	data.extend(fit(train_set1)) # preenche com a média e variância de cada feat para a classe Atacante
 	data.extend(fit(train_set2)) # preenche com a média e variância de cada feat para a classe Defensor
-
 	data = np.asarray(data).reshape(2,14)
-
 	# constroi a tabela com os dados do set de treino
 	classifier = pd.DataFrame(data,index=['Atacante','Defensor'],
 		columns=  [['Altura','Altura',
@@ -68,24 +56,19 @@ def constroy_model(dataset):
 			'mean','var',
 			'mean','var']])
 
-
 	return classifier
 
 ############################################################################################################################
 # Esta função recebe uma classe e um vetor de valores das características e calcula a posterior para a classe em questão.  #
 ############################################################################################################################
 def compute_posterior(model,category,feats):
-
 	last = ''
 	gnb = 1 # gaussianNaiveBayes probability distribuition value of some feat v to a given class Ck.
 	i = 0
 	for column in model.columns:
-
 		if column[0] == last:
 			continue
-		#print(column[0])
 		feat = feats[i] # vetor de valores das caracteristicas selecionadas
-		#print(feat)
 		mean, var = model.loc[category,column[0]]
 		gnb *= gaussianNB(mean,var,feat)
 		i += 1
@@ -98,7 +81,6 @@ def compute_posterior(model,category,feats):
 # Utiliza o método Gaussian Naive Bayes. Assume se que os dados possuem uma distribuição normal.    #
 #####################################################################################################
 def gaussianNB(mean,var,feat):
-
 	pi = m.pi
 	pot = -(m.pow((feat-mean),2))/(2*var)
 	output = (1/(m.sqrt(2*pi*var)))*m.exp(pot)
@@ -117,13 +99,11 @@ def compute_evidence(prob1,prob2,label1,label2):
 # Retorna uma dada classe baseado no funcionamento do classificador.                                    #
 #########################################################################################################
 def predict(classifier,feat_values):
-
 	#evidence = compute_evidence(prob_striker,prob_defensor,'Atacante','Defensor')
 	striker = compute_posterior(classifier,'Atacante',feat_values)  # calcula a posterior para a classe Atacante.
 	striker = striker*(0.5) # multiplcia pelo valor de probabilidade de ser a classe atacante(50%) Prior.
 	defensor = compute_posterior(classifier,'Defensor',feat_values) # calcula a posterior para a classe Defensor.
 	defensor = defensor*(0.5) # multiplcia pelo valor de probabilidade de ser a classe defensor(50%) Prior.
-
 	if striker > defensor:
 		return 'Atacante'
 	else:
@@ -133,34 +113,23 @@ def predict(classifier,feat_values):
 # Esta função classifica uma dada amostra baseado nas feats selecionadas. #
 ###########################################################################
 def classify(model,dataset):
-
 	feats = ['Altura','Tecnica','Passe','Chute','Forca','Velocidade','Drible'] # características selecionadas para o classificador
-
 	#for index in dataset.index:
-
 	feats_values = list(dataset[feats])
-
 	labels = predict(model,feats_values)
-	#print(labels)
-
 	return labels
 
 ####################################################
 # Esta função calcula a acurácia do classificador. #
 ####################################################
 def accuracy(output, labels):
-
 	count = 0
 	total = len(labels) 										#melhorar esta funçao
-
 	for i in range(0,total):
-
 		if output[i] == labels[i]:
 			count += 1
-
-	print("Acuracia foi de {0}\n\n".format(count/total))
-
-	return count/total
+    print("Acuracia foi de {0}\n\n".format(count/total))
+    return count/total
 
 '''
 def k_folders(data,folds):
@@ -185,30 +154,20 @@ def k_folders(data,folds):
 '''
 
 def leave_one_out(data):
-
 	output = []
 	for i in range(0,len(data)):
-
 		one_out = data.iloc[i]
-		#print(one_out)
 		aux = data.copy()
 		aux.drop(i)
-		model = constrol_model(aux)
+		model = build_model(aux)
 		output.append(classify(model,one_out))
-		#print(len(output))
-
-	#print(output)
 	print("Leave One Out")
 	accuracy(output,data['Classe'].values)
 
-
 def main():
-
 	dataframe = pd.read_csv("C:/Users/danil/Documents/trab3IA/treino.csv")
 	new_dataframe = pd.read_csv("C:/Users/danil/Documents/trab3IA/nao_classificados.csv")
-
 	#output = classify(classifier,test_set)
-
 	leave_one_out(dataframe)
 	#k_folders(dataframe,10)
 	'''
@@ -220,7 +179,5 @@ def main():
 	'''
 	#print(test_set)
 
-
 if __name__ == "__main__":
-
 	main()
--- naive-bayes/naivenbayes.py
+++ naive-bayes/naivenbayes.py
@@ -7,20 +7,15 @@ from sklearn.utils import shuffle
 import os
 
 #feats = ['Idade','Altura','Tecnica','Passe','Chute','Forca','Velocidade','Drible']
-
-def avaliaçao(cnf_matrix):
-
+def avaliacao(cnf_matrix):
     vp = cnf_matrix[0][0] # VP
     fn = cnf_matrix[0][1] # FN
     fp = cnf_matrix[1][0] # FP
     vn = cnf_matrix[1][1] # VN
-
     acuracia = (vp+vn)/(vp+vn+fp+fn) # perc de acertos total
     sensibilidade = vp/(vp+fn) # perc de positivos acertados # recall
     especificidade = vn/(vn+fp) # perc de negativos acertados
     precisao = vp/(vp+fp) # prob de repetir a acuracia
-
-
     print ("acuracia = ", acuracia)
     print ("sensibilidade = ", sensibilidade)
     print ("especificidade = ", especificidade)
@@ -28,55 +23,32 @@ def avaliaçao(cnf_matrix):
 
     #return acuracia, sensibilidade
 
-
-
 def cross_validation(dataSet,labels):
-
 	model = GaussianNB()
-
 	model.fit(x,y)
-
 	resultados = cross_val_predict(model, dataSet, labels, cv=15)
 	result = accuracy_score(y,resultados)
 	return result
 
 def normal_validation(dataSet,labels):
-
 	fraction=[110,100,90,80,70]
-
 	results = []
-
 	model = GaussianNB()
-
 	for fract in fraction:
-
 		treino = dataSet[:fract]
 		teste = dataSet[fract:]
 		treino_labels = labels[:fract]
 		teste_labels = labels[fract:]
-
 		model.fit(treino,treino_labels)
-
 		predicted = model.predict(teste)
-
 		results.append(accuracy_score(teste_labels,predicted))
-
 	results = np.asarray(results)
 	soma = results.sum()
-
 	soma = soma/5
-
 	print(soma)
 
-
-	#print(len(dataSet))
-	#print(len(dataSet[:30]))
-
-
 def classify(model,data):
-
 	predicted = model.predict(data)
-
 	return predicted
 
 feats = ['Altura','Tecnica','Passe','Chute','Forca','Velocidade','Drible']
@@ -91,9 +63,7 @@ y = dataset['Classe'].values
 
 
 def create_model(x,y):
-
 	model = GaussianNB()
-
 	model.fit(x,y)
 
 	return model
@@ -102,9 +72,7 @@ def main():
 
 	path = "C:/Users/danil/Documents/trab3IA/nao_classificados.csv"
 	feats = ['Idade','Altura','Tecnica','Passe','Chute','Forca','Velocidade','Drible']
-
 	dataset1 = pd.read_csv("C:/Users/danil/Documents/trab3IA/treino.csv")
-
 	dataset2 = pd.read_csv(path)
 	print(dataset1['Altura'].mean())
 	print(dataset1['Altura'].var())
@@ -117,9 +85,9 @@ def main():
 
 	dataset2.to_csv("C:/Users/danil/Documents/trab3IA/classificados2.csv")
 	'''
+
 if __name__ == "__main__":
 	main()
-
 #print(cross_validation(x,y))
 #print(normal_validation(x,y))
 #print(model.score(x,y))
